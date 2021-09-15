a65c87d87cbeeb7ff47f8a0471a88e9529bb75ff
JasperBraakman <58559476+JasperBraakman@users.noreply.github.com>
2021-09-15 08:54:04 +0200

    Create 2_air_quality.py

---
+++ py2_Traffic/2_air_quality.py
@@ -0,0 +1,16 @@
+###############################################################################################
+# This is the auxiliary file for the practical assignment to learn python for API´s.          #
+# Prepared by the Spatial Information Laboratory - Vrije Universiteit Amsterdam               #
+# Maurice de Kleijn, Devi Brands                                                              #
+###############################################################################################
+
+# Step 1: We first need to identify stations that are in close proximity to the locations of our car data sample.
+# Remove the hastag below
+# all_stations=[{'number':'NL49007'},{'number':'NL10641'}]
+
+# Step 2: Now create an average score of all the measurements for both compounds for both stations and add this score to the df. 
+# This step is simular to what we did in "timeseries.py" refer back to that code for help.
+# Hint: you can either write the code four times, for each station and compound seperate or you can use
+# a loop inbedded in a loop. One to loop over the stations and one to loop over te compounds.
+
+# Step 3: Transport the df into a CSV file
